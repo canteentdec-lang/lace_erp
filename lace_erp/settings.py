8256992f8db5451d52a19cd82476022f6fc24ea9
Krish Jariwala <krishjariwala10@gmail.com>
2026-03-20 16:17:12 +0530

    1changes

--- lace_erp/settings.py
+++ lace_erp/settings.py
@@ -23,6 +23,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 ROOT_URLCONF = 'lace_erp.urls'
 TEMPLATES = [{
